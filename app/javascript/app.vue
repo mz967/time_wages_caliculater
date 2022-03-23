cf36840d7c84cd01aa21d695ac71e5551e594d6c
satoshi796 <west314796@gmail.com>
2022-03-23 13:26:03 +0900

    Fix: Herokuデプロイのための変更36

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app" class="text-shadow  content-center text-blue-700">
-    <TheHeader class=""/>
+    <TheHeader/>
     <main class="container mx-auto px-5 flex flex-col min-h-screen">
       <div  class="w-full h-24">
         <Message class="ml-20"/>
       </div>
       <router-view />
     </main>
-    <TheFooter class=""/>
+    <TheFooter/>
   </div>
 </template>
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,24 +6,24 @@ environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 // module.exports = environment
 
-// const customConfig = {
-//   resolve: {
-//     fallback: {
-//       dgram: false,
-//       fs: false,
-//       net: false,
-//       tls: false,
-//       child_process: false
-//     }
-//   }
-// };
+const customConfig = {
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  }
+};
 
-// environment.config.delete('node.dgram')
-// environment.config.delete('node.fs')
-// environment.config.delete('node.net')
-// environment.config.delete('node.tls')
-// environment.config.delete('node.child_process')
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
 
-// environment.config.merge(customConfig);
+environment.config.merge(customConfig);
 
 module.exports = environment

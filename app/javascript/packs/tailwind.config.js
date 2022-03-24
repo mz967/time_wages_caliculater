434fe05befed0fea855b735b5379da95e4a6a75f
satoshi796 <west314796@gmail.com>
2022-03-24 13:14:30 +0900

    Fix: Herokuデプロイのための変更42

--- app/javascript/packs/tailwind.config.js
+++ app/javascript/packs/tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
     './app/javascript/pages/**/*.vue',
     './app/javascript/pages/*.vue'
   ],
+  preserveHtmlElements: false,
+  options: {
+    defaultExtractor: (content) => {
+      const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+      return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+    },
+    whitelist: [ /-(leave|enter|appear)(|-(to|from|active))$/, /^(?!cursor-move).+-move$/, /^router-link(|-exact)-active$/ ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
